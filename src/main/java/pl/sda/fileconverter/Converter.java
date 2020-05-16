349b7970d16234d76ea640f14d561c246cf6d8c1
pawel-matyaszczyk <pawel.matyaszczyk@gmail.com>
2020-05-16 10:25:03 +0200

    add json file

---
+++ src/main/java/pl/sda/fileconverter/Converter.java
@@ -0,0 +1,11 @@
+package pl.sda.fileconverter;
+
+/**
+ * @author Paweł Matyaszczyk
+ */
+public class Converter {
+    public void convert(String sourceFilePath, String outputFilePath){
+
+    }
+
+}
--- src/main/java/pl/sda/fileconverter/Main.java
+++ src/main/java/pl/sda/fileconverter/Main.java
@@ -5,6 +5,9 @@ package pl.sda.fileconverter;
  */
 public class Main {
     public static void main(String[] args) {
-
+        Converter converter = new Converter();
+        String sourceFilePath = "test.json";
+        String outputFilePath = "test.xlsx";
+        converter.convert(sourceFilePath,outputFilePath);
     }
 }
